4d0b1c767c2c6a50414746b37209fbee35564e94
alexchan38168 <87537680+alexchan38168@users.noreply.github.com>
2022-11-28 21:22:26 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,20 @@
+function myf(){
+    x=document.getElementById("demo");
+    x.innerHTML="noob"; 
+}
+function myf2(){
+    x=document.getElementById("demo2");
+    x.innerHTML="sb"; 
+}
+function myf3(){
+    x=document.getElementById("demo3");
+    x.innerHTML="rubbish"; 
+}
+function myf4(){
+    x=document.getElementById("demo");
+    y=document.getElementById("demo2");
+    z=document.getElementById("demo3");
+    x.innerHTML="ken"; 
+    y.innerHTML="dickey mouse"; 
+    z.innerHTML="queeny wrong"; 
+}
