11fa2c8fb8b643a3178ad78318b96dffa8ec931f
alexchan38168 <87537680+alexchan38168@users.noreply.github.com>
2022-12-01 19:30:28 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,26 +1,46 @@
 function myf(){
-    x=document.getElementById("demo");
-    x.innerHTML="noob"; 
-}
-function myf2(){
-    x=document.getElementById("demo2");
-    x.innerHTML="sb"; 
-}
-function myf3(){
-    x=document.getElementById("demo3");
-    x.innerHTML="rubbish"; 
-}
-function myf4(){
-    x=document.getElementById("demo");
-    y=document.getElementById("demo2");
-    z=document.getElementById("demo3");
-    x.innerHTML="ken"; 
-    y.innerHTML="dickey mouse"; 
-    z.innerHTML="queeny wrong"; 
-}
-function myf5(){
     let currentDate = new Date();
     let time = currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
-    x=document.getElementById("demo5");
+    x=document.getElementById("demo");
     x.innerHTML=time; 
+}
+
+function f(){
+    let height = document.getElementById("height").value; 
+    let weight = document.getElementById("weight").value; 
+    document.getElementById("y").src = "./image/f9ed3758550807bd9eaeaa0b27fff7b.jpg";
+
+    let bmi = weight/(height*height);
+    if (bmi>=27){
+        x=document.getElementById("x");
+        x.innerHTML="Your BMI is "&& bmi.toFixed(1);
+        alert("very fat boy.")
+    }
+    else if (bmi>=24){
+        let height = document.getElementById("height").value; 
+        let weight = document.getElementById("weight").value; 
+        let bmi = weight/(height*height);
+        x=document.getElementById("x");
+        x.innerHTML="Your BMI is "&& bmi.toFixed(1);
+        document.getElementById("y").src = "./image/cc52fe9a142d4db79c11cbdeb676856.jpg";
+        alert("fat boy.")
+    }
+    else if (bmi>=18.5){
+        let height = document.getElementById("height").value; 
+        let weight = document.getElementById("weight").value; 
+        let bmi = weight/(height*height);
+        x=document.getElementById("x");
+        x.innerHTML="Your BMI is " && bmi.toFixed(1);
+        document.getElementById("y").src = "./image/f9ed3758550807bd9eaeaa0b27fff7b.jpg";
+        alert("normal boy.")
+    }
+    else {
+        let height = document.getElementById("height").value; 
+        let weight = document.getElementById("weight").value; 
+        let bmi = weight/(height*height);
+        x=document.getElementById("x");
+        x.innerHTML="Your BMI is "&& bmi.toFixed(1);
+        document.getElementById("y").src = "./image/cc52fe9a142d4db79c11cbdeb676856.jpg";
+        alert("weak boy.")
+    }
 }
